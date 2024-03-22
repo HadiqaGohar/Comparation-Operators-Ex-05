091cb35c0655d2b19d1bd5ff9dc5a11b9c3cbc1a
HadiqaGohar <160311280+HadiqaGohar@users.noreply.github.com>
2024-03-22 08:25:19 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+// COMPARATION OPERATOR
+//WEATHER
+var weather = "cloudy";
+if (weather === "raining") {
+    console.log("wear a raincoat.");
+}
+else if (weather === "cloudy") {
+    console.log("wear a light jacket");
+}
+else {
+    console.log("wear sunglasses");
+}
---
+++ main.ts
@@ -0,0 +1,10 @@
+// COMPARATION OPERATOR
+//WEATHER
+let weather = "cloudy";
+if (weather === "raining") {
+    console.log ("wear a raincoat.")
+} else if (weather === "cloudy") {
+    console.log ("wear a light jacket");
+} else {
+    console.log ("wear sunglasses");
+}
